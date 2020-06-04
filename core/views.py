aef86c185a5d35474bb0c8e7285556a24bc5bf59
luizotaviobione <luix195@gmail.com>
2020-06-04 20:01:06 -0300

    incluido inserção de eventos da agenda

--- core/views.py
+++ core/views.py
@@ -17,6 +17,10 @@ def logout_user(request):
     logout(request)
     return redirect('/')
 
+@login_required(login_url='/login/')
+def evento(request):
+    return render(request, 'evento.html')
+
 def submit_login(request):
     if request.POST:
         username = request.POST.get('username')
@@ -44,3 +48,14 @@ def lista_eventos(request):
     dados = {'eventos':evento}
     print('os dados são {}'.format(dados))
     return render(request, 'agenda.html', dados)
+
+@login_required(login_url='/login/')
+def submit_evento(request):
+    if request.POST:
+        titulo = request.POST.get('titulo')
+        data_evento = request.POST.get('data_evento')
+        descricao = request.POST.get('descricao')
+        usuario = request.user
+        Evento.objects.create(titulo=titulo, data_evento=data_evento, descricao=descricao, usuario= usuario)
+
+    return redirect('/')

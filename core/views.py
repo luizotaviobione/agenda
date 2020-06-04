ed423c33c4e4528271f3ab60a05ac81d84b1887e
luizotaviobione <luix195@gmail.com>
2020-06-04 12:53:57 -0300

    criada listagem de eventos

--- core/views.py
+++ core/views.py
@@ -1,3 +1,19 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from core.models import Evento
 
 # Create your views here.
+
+# def index(request):
+#     return redirect('/agenda/')
+
+def lista_eventos(request):
+
+    #evento = Evento.objects.get(id=1)
+    evento = Evento.objects.all()
+    #usuario = request.user
+    #evento = Evento.objects.filter(usuario=usuario)
+    print('eventos = {}'.format(evento))
+    print(type(evento))
+    dados = {'eventos':evento}
+    print('os dados são {}'.format(dados))
+    return render(request, 'agenda.html', dados)
